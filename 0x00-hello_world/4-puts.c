e9332cbf7d08b9a7ab9a530830d1477e81520939
Peterp254 <pt.kariuki@gmail.com>
2023-02-16 15:00:16 -0800

    the function puts/executable named cisfun.

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#!/bin/bash
+#include <stdio.h>
+/**
+ * main - program printing line using puts
+ *
+ * Return: Always 0 (succes)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
