3dba809a3a2295350588c6bfa407b04eb0ee57e4
Peterp254 <pt.kariuki@gmail.com>
2023-02-17 12:32:44 -0800

    all the numbers of base 16 in lowercase, followed by a new line.

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,20 +1,22 @@
 #include <stdio.h>
+
 /**
- * main -prints all possible combinations of single-digit numbers.
+ * main - Prints all the numbers of base 16 in lowercase.
  *
- * Return: Always 0 (success)
+ * Return: Always 0.
  */
 int main(void)
 {
-int num;
-for (num = 0; num <= 9; num++)
-{
-putchar((num % 10) + '0');
-if (num == 9)
-continue;
-putchar(',');
-putchar(' ');
+	int num;
+	char letter;
 
-return (0);
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+
+	for (letter = 'a'; letter <= 'f'; letter++)
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0);
 }
-
