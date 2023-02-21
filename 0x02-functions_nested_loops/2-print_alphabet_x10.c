5a4db848c5f01894a57f61de104a188ec69d6487
Peterp254 <pt.kariuki@gmail.com>
2023-02-21 13:18:52 -0800

    Write a program that prints _putchar

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,21 +1,21 @@
 #include "main.h"
 
 
-	/**
-	 * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
-	 * followed by a new line
-	 */
-	void print_alphabet_x10(void)
-	{
-		char ch;
-		int i;
+/**
+* print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
+* followed by a new line
+*/
+void print_alphabet_x10(void)
+{
+	char ch;
+	int i;
 
 
-		i = 0;
+	i = 0;
 
 
 		while (i < 10)
-		{
+	{
 			ch = 'a';
 			while (ch <= 'z')
 			{
@@ -24,6 +24,6 @@
 			}
 			_putchar('\n');
 			i++;
-		}
 	}
+}
 
